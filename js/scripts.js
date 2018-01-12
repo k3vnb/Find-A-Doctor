78c7a6ffb5b0c97dc721faf82b39604573f87c94
Kevin Boyle <papershack@gmail.com>
2018-01-12 11:49:04 -0800

    show parse test 3: response.data[i].profile, where lives name info of doctor

--- js/scripts.js
+++ js/scripts.js
@@ -15,7 +15,7 @@ export class DoctorSearch {
       },
       success: function(response) {
         console.log(this.url);
-        console.log(response.data[0]);
+        console.log(response.data[0].profile);
         console.log("success");
         // success(response);
       },
