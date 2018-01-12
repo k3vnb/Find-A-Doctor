80e84b06f29efeeb931aa47cb0d4a09bedadb69c
Kevin Boyle <papershack@gmail.com>
2018-01-12 11:18:09 -0800

    add random char to API key to confirm error

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@ export class DoctorSearch {
 
   getDocResults(success, error) {
     $.ajax({
-      url: `https://api.betterdoctor.com/2016-03-01/doctors?name=susan&query=rash&location=45.5231%2C-122.6765%2C10&user_location=45.5231%2C-122.6765&skip=0&limit=10&user_key=${apiKey}`,
+      url: `https://api.betterdoctor.com/2016-03-01/doctors?name=susan&query=rash&location=45.5231%2C-122.6765%2C10&user_location=45.5231%2C-122.6765&skip=0&limit=10&user_key=s${apiKey}`,
       type: 'GET',
       data: {
         format: 'json'
@@ -18,6 +18,7 @@ export class DoctorSearch {
         // success(response);
       },
       error: function() {
+        console.log("error");
         // error(response);
       }
     });//$.ajax
