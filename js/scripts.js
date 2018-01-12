a2a8956741d218172450b5d5d72e1d3a75b5d39e
Kevin Boyle <papershack@gmail.com>
2018-01-12 11:20:59 -0800

    add variables to url, remove hard-coded values for search parameters

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@ export class DoctorSearch {
 
   getDocResults(success, error) {
     $.ajax({
-      url: `https://api.betterdoctor.com/2016-03-01/doctors?name=susan&query=rash&location=45.5231%2C-122.6765%2C10&user_location=45.5231%2C-122.6765&skip=0&limit=10&user_key=s${apiKey}`,
+      url: `https://api.betterdoctor.com/2016-03-01/doctors?name=${this.name}&query=${this.condition}&location=45.5231%2C-122.6765%2C10&user_location=45.5231%2C-122.6765&skip=0&limit=10&user_key=${apiKey}`,
       type: 'GET',
       data: {
         format: 'json'
