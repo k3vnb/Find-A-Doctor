dddd69ebf98da59be657d3207b8445bced523bd0
Kevin Boyle <papershack@gmail.com>
2018-01-12 17:16:56 -0800

    add href style to URLs, add some more styling

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@ export class DoctorSearch {
 
   getDocResults(success, error) {
     $.ajax({
-      url: `https://api.betterdoctor.com/2016-03-01/doctors?name=${this.name}&query=${this.condition}&location=45.5231%2C-122.6765%2C10&user_location=45.5231%2C-122.6765&skip=0&limit=10&user_key=${apiKey}`,
+      url: `https://api.betterdoctor.com/2016-03-01/doctors?name=${this.name}&query=${this.condition}&location=45.5231%2C-122.6765%2C10&user_location=45.5231%2C-122.6765&skip=0&limit=50&user_key=${apiKey}`,
       type: 'GET',
       data: {
         format: 'json'
@@ -16,11 +16,11 @@ export class DoctorSearch {
       success: function(response) {
         let responseArray = [];
 
-        function defineURL(a){
-          if (a === undefined) {
+        function defineURL(site){
+          if (site === undefined) {
             return "none provided";
           } else {
-            return a;
+            return `<a href="${site}">${site}</a>`;
           }
         }
 
